534c2598a08dd059f126ff3ce6659814893ddbc6
Patriceboahen <36483485+Patriceboahen@users.noreply.github.com>
2018-03-13 13:44:23 -0400

    Create application.js

---
+++ application.js
@@ -0,0 +1,50 @@
+var config = {
+    apiKey: "AIzaSyD2_S2nB82cs2mN7v31VxSjB6v5t3XVjQs",
+    authDomain: "patrice-project.firebaseapp.com",
+    databaseURL: "https://patrice-project.firebaseio.com",
+    projectId: "patrice-project",
+    storageBucket: "patrice-project.appspot.com",
+    messagingSenderId: "326285593206"
+};
+
+// Initialize your Firebase app
+firebase.initializeApp(config);
+
+// Reference to the comment's object in your Firebase database
+var comments = firebase.database().ref("comments");
+
+
+// Save a new comment to the database, using the input in the form
+var submitComment = function () {
+
+  var title = $("#yourName").val();
+  var remark = $("#yourComment").val();
+  var link = $("#yourLink").val();
+
+  comments.push({
+    "title": title,
+    "remark": remark,
+    "link": link
+  });
+};
+
+comments.limitToLast(10).on('child_added', function(childSnapshot) {
+
+  comment = childSnapshot.val();
+
+  $("#title").prepend('<tr><td>' + comment.title + '</td></tr>' + '<br> <br>')
+  $("#remark").prepend('<tr><td>' + comment.remark + '</td></tr>' + '<br> <br>')
+  $("#link").prepend('<tr><td><img id="image"></td></tr>' + '<br> <br>')
+
+  
+  var altWords = 'No \n Image'  
+  // Make the link actually work and direct to the URL provided
+  $("#image").attr("src", comment.link)
+  $("#image").attr("alt", altWords) 
+});
+
+
+// When the window is fully loaded, call this function.
+$(window).load(function () {
+  $("#commentForm").submit(submitComment);
+});
